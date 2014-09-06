731fa19a4eb7c94683e0cd660c66b13dc2c4dd0e
destiny1020 <destiny.jiang@gmail.com>
2014-09-06 11:58:23 +0800

    workflow finish

--- gulpfile.js
+++ gulpfile.js
@@ -2,32 +2,113 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
     browserify = require('gulp-browserify'),
-    concat = require('gulp-concat');
+    compass = require('gulp-compass'),
+    concat = require('gulp-concat'),
+    gulpif = require('gulp-if'),
+    uglify = require('gulp-uglify'),
+    minifyHTML = require('gulp-minify-html'),
+    minifyJSON = require('gulp-jsonminify'),
+    // imagemin = require('gulp-imagemin'),
+    // pngcrush = require('imagemin-pngcrush'),
+    connect = require('gulp-connect');
 
-// gulp.task('log', function() {
-//     gutil.log('workflows logging');
-// });
+var env, coffeeSources, jsSources, sassSources, htmlSources, jsonSources,
+    outputDir, 
+    sassStyle;
 
-// * is available
-var coffeeSources = ['components/coffee/*.coffee'];
-var jsSources = [
-    'components/scripts/rclick.js',
-    'components/scripts/pixgrid.js',
-    'components/scripts/tagline.js',
-    'components/scripts/template.js'
+env = process.env.NODE_ENV || 'development';
+
+if(env === 'development') {
+  outputDir = 'builds/development/';
+  sassStyle = 'expanded';
+} else {
+  outputDir = 'builds/production/';
+  sassStyle = 'compressed';
+}
+
+coffeeSources = ['components/coffee/tagline.coffee'];
+jsSources = [
+  'components/scripts/rclick.js',
+  'components/scripts/pixgrid.js',
+  'components/scripts/tagline.js',
+  'components/scripts/template.js'
 ];
+sassSources = ['components/sass/style.scss'];
+htmlSources = [outputDir + '*.html'];
+jsonSources = [outputDir + 'js/*.json'];
 
 gulp.task('coffee', function() {
-    gulp.src(coffeeSources)
-        // bare is an option in coffescript itself
-        .pipe(coffee({bare: true})
-            .on('error', gutil.log))
-        .pipe(gulp.dest('components/scripts'));
+  gulp.src(coffeeSources)
+    .pipe(coffee({ bare: true })
+      .on('error', gutil.log))
+    .pipe(gulp.dest('components/scripts'))
 });
 
 gulp.task('js', function() {
-    gulp.src(jsSources)
-        .pipe(concat('script.js'))
-        .pipe(browserify())
-        .pipe(gulp.dest('builds/development/js'));
+  gulp.src(jsSources)
+    .pipe(concat('script.js'))
+    .pipe(browserify())
+    .pipe(gulpif(env === 'production', uglify()))
+    .pipe(gulp.dest(outputDir + 'js'))
+    .pipe(connect.reload());
 });
+
+gulp.task('compass', function() {
+  gulp.src(sassSources)
+    .pipe(compass({
+      sass: 'components/sass',
+      image: outputDir + 'images',
+      style: sassStyle
+    })
+    .on('error', gutil.log))
+    .pipe(gulp.dest(outputDir + 'css'))
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function() {
+  gulp.watch(coffeeSources, ['coffee']);
+  gulp.watch(jsSources, ['js']);
+  gulp.watch('components/sass/*.scss', ['compass']);
+  // since at first there is no html in the production folder
+  gulp.watch('builds/development/*.html', ['html']);
+  // since at first there is no json file in the production folder
+  gulp.watch('builds/development/js/*.json', ['json']);
+  // since at first there is no image file in the production folder
+  gulp.watch('builds/development/images/**/*.*', ['images']);
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    root: outputDir,
+    livereload: true
+  });
+});
+
+gulp.task('html', function() {
+  // since at first there is no html in the production folder
+  gulp.src('builds/development/*.html')
+    .pipe(gulpif(env === 'production', minifyHTML()))
+    .pipe(gulpif(env === 'production', gulp.dest(outputDir)))
+    .pipe(connect.reload());
+});
+
+gulp.task('images', function() {
+  gulp.src('builds/development/images/**/*.*')
+    // .pipe(gulpif(env === 'production', imagemin({
+    //   progressive: true,
+    //   svgoPlugins: [{ removeViewBox: false }],
+    //   use: [pngcrush()]
+    // })))
+    .pipe(gulpif(env === 'production', gulp.dest(outputDir + 'images')))
+    .pipe(connect.reload())
+});
+
+gulp.task('json', function() {
+  // since at first there is no json file in the production folder
+  gulp.src('builds/development/js/*.json')
+    .pipe(gulpif(env === 'production', minifyJSON()))
+    .pipe(gulpif(env === 'production', gulp.dest('builds/production/js')))
+    .pipe(connect.reload());
+});
+
+gulp.task('default', ['html', 'json', 'coffee', 'js', 'compass', 'images', 'connect', 'watch']);
